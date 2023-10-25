48f4f71031cfa96bfc816f20c3769324db00b40a
maieulchevalier <maieul.chevalier@gmail.com>
2023-10-25 22:06:12 +0200

    refactor

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,31 @@
 import { defineConfig } from "vite";
+import pkg from "./package.json";
 import { qwikVite } from "@builder.io/qwik/optimizer";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+const { dependencies = {}, peerDependencies = {} } = pkg as any;
+const makeRegex = (dep) => new RegExp(`^${dep}(/.*)?$`);
+const excludeAll = (obj) => Object.keys(obj).map(makeRegex);
 
 export default defineConfig(() => {
   return {
     build: {
       target: "es2020",
-      emptyOutDir: false,
       lib: {
         entry: ["./src/index.ts", "./src/recma-jsx-rewrite-qwik.ts"],
         formats: ["es", "cjs"],
-        fileName: (format, entryName) =>
-          `${entryName}.${format === "es" ? "mjs" : "cjs"}`,
+        fileName: (format) => `index.qwik.${format === "es" ? "mjs" : "cjs"}`,
       },
       rollupOptions: {
-        external: ["unified"],
+        // externalize deps that shouldn't be bundled into the library
+        external: [
+          "unified",
+          /^node:.*/,
+          ...excludeAll(dependencies),
+          ...excludeAll(peerDependencies),
+        ],
       },
     },
-    plugins: [qwikVite()],
+    plugins: [qwikVite(), tsconfigPaths()],
   };
 });
